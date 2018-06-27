e73a04f42b469abcc0ba6c366d60fc5be2a3375a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-06-27 16:15:47 -0400

    Fixing module declaration

--- types/quick.db/index.d.ts
+++ types/quick.db/index.d.ts
@@ -9,29 +9,32 @@ type OptionsWithTarget = {
 };
 type DataArray = Array<{ID: string, data: any}>;
 
-// TODO is any the right type for data elements? They will be null if never set
-// before, but can't be set to undefined (I think. Need to reproduce) and also
-// can't be functions (I assume).
+declare module quick.db {
 
-export function createWebview(password: string, port?: number): undefined;
-export function set(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
-export function fetch(ID: string, options?: OptionsWithTarget): Promise<any>;
-export function fetchAll(options?: { table?: string }): Promise<DataArray>;
-export function startsWith(text: string, options?: { sort?: string, table?: string }): Promise<DataArray>;
-declare function _delete(ID: string, options?: { table?: string }): Promise<boolean>;
-export { _delete as delete };
-export function add(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
-export function subtract(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
-export function push(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
+    // TODO is any the right type for data elements? They will be null if never set
+    // before, but can't be set to undefined (I think. Need to reproduce) and also
+    // can't be functions (I assume).
 
-export class Table
-{
-    readonly name: string;
-    fetch(Id: string, options?: OptionsWithTarget): Promise<any>;
-    fetchAll(options?: { table?: string }): Promise<DataArray>;
-    startsWith(text: string, options?: { sort?: string, table?: string }): Promise<DataArray>;
-    delete(ID: string, options?: { table?: string }): Promise<boolean>;
-    add(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
-    subtract(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
-    push(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
+    export function createWebview(password: string, port?: number): undefined;
+    export function set(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
+    export function fetch(ID: string, options?: OptionsWithTarget): Promise<any>;
+    export function fetchAll(options?: { table?: string }): Promise<DataArray>;
+    export function startsWith(text: string, options?: { sort?: string, table?: string }): Promise<DataArray>;
+    function _delete(ID: string, options?: { table?: string }): Promise<boolean>;
+    export { _delete as delete };
+    export function add(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
+    export function subtract(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
+    export function push(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
+
+    export class Table
+    {
+        readonly name: string;
+        fetch(Id: string, options?: OptionsWithTarget): Promise<any>;
+        fetchAll(options?: { table?: string }): Promise<DataArray>;
+        startsWith(text: string, options?: { sort?: string, table?: string }): Promise<DataArray>;
+        delete(ID: string, options?: { table?: string }): Promise<boolean>;
+        add(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
+        subtract(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
+        push(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
+    }
 }
