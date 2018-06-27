8afc5942d6442add3c4ef1bd0a9be868a58c0818
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-06-27 12:59:47 -0400

    Defined all functions, but delete() is a problem

--- types/quick.db/index.d.ts
+++ types/quick.db/index.d.ts
@@ -1,39 +1,22 @@
 // Type definitions for quick.db 6.3
 // Project: https://github.com/TrueXPixels/quick.db#readme
-// Definitions by: My Self <https://github.com/me>
+// Definitions by: Nat Quayle Nelson <https://github.com/NQNStudios>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
+type OptionsWithTarget = {
+    target?: string;
+    table?: string;
+};
+type DataArray = Array<{ID: string, data: any}>;
 
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+export function createWebview(password: string, port?: number): undefined;
+export function set(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
+export function fetch(ID: string, options?: OptionsWithTarget): Promise<any>;
+export function fetchAll(options?: { table?: string }): Promise<DataArray>;
+export function startsWith(text: string, options?: { sort?: string, table?: string }): Promise<DataArray>;
+export function delete(ID: string, options?: { table?: string }): Promise<boolean>;
+export function add(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
+export function subtract(ID: string, value: number, options?: OptionsWithTarget): Promise<any>;
+export function push(ID: string, data: any, options?: OptionsWithTarget): Promise<any>;
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
-
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    export function foo(): void;
-}
+// TODO table class
